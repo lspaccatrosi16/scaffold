fca723a40da0950db5f6b804185af6db71a07d69
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-10-22 09:37:41 +0200

    update go-cli to use lib

--- templates/go-cli/cmd/cli/main.go
+++ templates/go-cli/cmd/cli/main.go
@@ -4,15 +4,15 @@ import (
 	"example.com/my/project/lib/error"
 	"example.com/my/project/lib/foo"
 	"example.com/my/project/lib/hello"
-	"example.com/my/project/lib/types"
+	"github.com/lspaccatrosi16/go-cli-tools/command"
 )
 
 func main() {
-	manager := types.NewManager()
+	manager := command.NewManager(command.ManagerConfig{Searchable: true})
 
 	manager.Register("hello", "Says hello to you", hello.Hello)
 	manager.Register("foo", "FooBarBiz", foo.Foo)
 	manager.Register("error", "Simulates an error being raised", error.Error)
 
-	manager.Gui()
+	manager.Tui()
 }
